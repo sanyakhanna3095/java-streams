6557ca7ad6cf201242bbc33249d1b3c7dd0e19be
sanyakhanna3095 <sanya1266.be21@chitkara.edu.in>
2025-03-31 10:03:27 +0530

    [Sanya] Add: Store student details in a binary file and retrieves it later

--- src/main/java/com/bridgelabz/javaStreams/studentDetails/StudentDataStream.java
+++ src/main/java/com/bridgelabz/javaStreams/studentDetails/StudentDataStream.java
@@ -1,10 +1,10 @@
-package com.bridgelabz.javaStreams;
+package com.bridgelabz.javaStreams.studentDetails;
 
 import java.io.*;
 
 public class StudentDataStream {
     public static void main(String[] args) {
-        String fileName = "/Users/sanyakhanna/Desktop/bridgelabz-workspace/Java-Streams/src/main/java/com/bridgelabz/javaStreams/student_data.bin";
+        String fileName = "/Users/sanyakhanna/Desktop/bridgelabz-workspace/Java-Streams/src/main/java/com/bridgelabz/javaStreams/studentDetails/student_data.bin";
 
         // Store student details in a binary file
         try (DataOutputStream dos = new DataOutputStream(new FileOutputStream(fileName))) {
